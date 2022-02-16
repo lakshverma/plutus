6f2779ffc8174a3d7b77df7c2f30ea39bd733da0
Lakshay Verma <lakshverma23@gmail.com>
2022-02-16 09:39:30 +0530

    feat(common): add type prop to allow customizing button type attribute, refactor existing type prop to colorVariant prop

--- src/common/form/Button.js
+++ src/common/form/Button.js
@@ -1,21 +1,23 @@
 import React from "react";
 
-const Button = ({ buttonText, type, width }) => {
+const Button = ({ buttonText, type, colorVariant, width }) => {
   const buttonClasses =
-    type === "primary"
+    colorVariant === "primary"
       ? "text-white bg-primary-blue-plutus"
-      : type === "secondary"
+      : colorVariant === "secondary"
       ? "text-primary-blue-plutus bg-skyblue-plutus"
-      : type === "inactive"
+      : colorVariant === "inactive"
       ? "text-primary-grey-plutus bg-lightgrey-plutus"
       : "";
   const widthClass =
     width === "lg"
       ? "w-[252px]"
       : "w-[178px]";
+
   return (
     <button
       className={`font-lato font-bold text-center ${widthClass} h-[46px] rounded ${buttonClasses}`}
+      type={type}
     >
       {buttonText}
     </button>
